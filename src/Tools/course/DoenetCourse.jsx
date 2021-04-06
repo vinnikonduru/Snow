675c9cfccfbacab26cc0cd8409ff4b11148f7caa
Vinitha Konduru <vinithak@umn.edu>
2021-04-06 11:28:30 -0500

    courseTool start

---
+++ src/Tools/course/DoenetCourse.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { nanoid } from "nanoid";
+// import Enrollment from "./Enrollment";
+import Tool from "../imports/Tool/Tool";
+
+
+export default function DoenetCourse(){
+    return (
+      <Tool>
+        <headerPanel title="Course"></headerPanel>
+        <mainPanel>
+         
+        </mainPanel>
+        <menuPanel>
+          Menu
+        </menuPanel>
+      </Tool>
+    );
+  }
+
