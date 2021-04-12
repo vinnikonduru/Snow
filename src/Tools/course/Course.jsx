d1d67f1ca316b339acb5d624d5c4682a959d49d8
Vinitha Konduru <vinithak@umn.edu>
2021-04-12 15:43:45 -0500

    CourseTool

--- src/Tools/course/Course.jsx
+++ src/Tools/course/Course.jsx
@@ -1,28 +1,156 @@
-import React, { useEffect } from 'react';
+/**
+ * External dependencies
+ */
+import React, { useEffect, useState, Suspense } from "react";
+
+import {
+  useHistory
+} from "react-router-dom";
+import {
+  atom,
+  useSetRecoilState,
+  useRecoilValue,
+  selector,
+  selectorFamily,
+  useRecoilValueLoadable,
+  useRecoilStateLoadable,
+  useRecoilCallback
+} from "recoil";
+import axios from "axios";
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+/**
+ * Internal dependencies
+ */
+import Drive, { 
+  folderDictionarySelector, 
+  globalSelectedNodesAtom, 
+  folderDictionary, 
+  clearDriveAndItemSelections,
+  fetchDrivesSelector,
+  encodeParams,
+  fetchDriveUsers,
+  fetchDrivesQuery,
+} from "../../_reactComponents/Drive/Drive";
+import { 
+  useAddItem,
+  useDeleteItem,
+  useRenameItem
+} from "../../_reactComponents/Drive/DriveActions";
+import { BreadcrumbContainer } from "../../_reactComponents/Breadcrumb";
+import Button from "../../_reactComponents/PanelHeaderComponents/Button";
+import DriveCards from "../../_reactComponents/Drive/DriveCards";
+import "../../_reactComponents/Drive/drivecard.css";
+import DoenetDriveCardMenu from "../../_reactComponents/Drive/DoenetDriveCardMenu";
+import '../../_utils/util.css';
+import GlobalFont from "../../Media/fonts/GlobalFont.js";
+import { driveColors, driveImages } from '../../_reactComponents/Drive/util';
 import Tool from '../_framework/Tool';
 import { useToolControlHelper } from '../_framework/ToolRoot';
-// import Drive from "../imports/Drive";
-// import { BreadcrumbContainer } from "../imports/Breadcrumb";
-import { useToast } from '../_framework/Toast';
-// import CollapseSection from "../imports/CollapseSection";
-// import SectionDivider from "../imports/PanelHeaderComponents/SectionDivider";
-
-export default function  Course() {
-  // console.log("=== DoenetExampleTool");
+import Toast, { useToast } from '../_framework/Toast';
+import {drivecardSelectedNodesAtom} from '../library/Library'
+function Container(props){
+  return <div
+  style={{
+      maxWidth: "850px",
+      // border: "1px red solid",
+      margin: "20px",
+  }
+  }
+  >
+      {props.children}
+  </div>
+}
+export default function  Course(props) {
+  console.log(">> course props",props);
 
   const { openOverlay, activateMenuPanel } = useToolControlHelper();
   const [toast, toastType] = useToast();
-
+  let routePathDriveId = "";
+  let urlParamsObj = Object.fromEntries(
+    new URLSearchParams(props.route.location.search)
+  );
+  if (urlParamsObj?.path !== undefined) {
+    [
+      routePathDriveId
+    ] = urlParamsObj.path.split(":");
+  }
   useEffect(() => {
     activateMenuPanel(1);
   }, [activateMenuPanel]);
+  const history = useHistory();
 
+  const driveCardSelection = ({item}) => {
+    let newParams = {};
+    newParams["path"] = `${item.driveId}:${item.driveId}:${item.driveId}:Drive`;
+    history.push("?" + encodeParams(newParams));
+  }
+  const setDrivecardSelection = useSetRecoilState(drivecardSelectedNodesAtom)
+  const clearSelections = useSetRecoilState(clearDriveAndItemSelections);
+
+  function cleardrivecardSelection(){
+    setDrivecardSelection([]);
+    // let newParams = {};
+    // newParams["path"] = `:::`;
+    // history.push("?" + encodeParams(newParams));
+  }
+  function useOutsideDriveSelector() {
+
+    let newParams = {};
+    newParams["path"] = `:::`;
+    history.push("?" + encodeParams(newParams));
+  }
+  let breadcrumbContainer = null;
+  if (routePathDriveId) {
+    breadcrumbContainer = <BreadcrumbContainer />;
+  }
+  
   return (
     <Tool>
      <headerPanel title="Course" />
+     <navPanel>
+      <GlobalFont/>
+      <div style={{marginBottom:"40px",height:"100vh"}} 
+       onClick={useOutsideDriveSelector} 
+       >
+      <Drive driveId={routePathDriveId}  foldersOnly={true} />
+      </div>
+      </navPanel>
      <mainPanel>
-       Course Tool
-      </mainPanel>
+     {breadcrumbContainer}
+        <div 
+        onClick={()=>{
+          clearSelections()
+        }}
+        className={routePathDriveId ? 'mainPanelStyle' : ''}
+        >
+          <Container>
+          <Drive types={['content','course']}  urlClickBehavior="select" 
+         doenetMLDoubleClickCallback={(info)=>{
+           openOverlay({type:"content",branchId: info.item.branchId,contentId:"",title: info.item.label});
+          }}
+          />
+          </Container>
+        
+
+        </div>
+       
+        <div 
+        onClick={
+          cleardrivecardSelection
+        }
+        tabIndex={0}
+        className={routePathDriveId ? '' : 'mainPanelStyle' }
+        >
+       <DriveCards
+       types={['course']}
+       subTypes={['Administrator']}
+       routePathDriveId={routePathDriveId}
+       driveDoubleClickCallback={({item})=>{driveCardSelection({item})}}
+       />
+        </div>     
+         </mainPanel>
     </Tool>
   );
 }
